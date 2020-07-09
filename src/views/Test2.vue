c6ef4772b9d59104ea564297a5bda9654bc17f70
ergouwang <330035385@qq.com>
2020-07-09 13:37:02 +0800

    书签页布局基本完成

--- src/views/Test2.vue
+++ src/views/Test2.vue
@@ -1,42 +1,77 @@
 <template>
-    <div class="root">
-        <span>书签分类</span>
-        <ul>
-            <li></li>
-        </ul>
+    <div class="home-container">
+        <Header/>
+        <div class="body-container">
+            <div class="panel-left">
+                <div class="title">
+                    <span>书签分类</span>
+                    <span>新增分类</span>
+                </div>
+                <div class="sort-detail">
+                    <ul>
+                        <li v-for="item in sortData" :key="item.id">
+
+                            {{item.sort_name}}
+                            <img src="../assets/edit.png" alt="编辑">
+                            <img src="../assets/delete.png" alt="删除">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="panel-right">
+                <List>
+                    <ListItem>
+                        <a href="#">www.baidu.com</a>
+                        <template slot="action">
+                            <li>
+                                <a href="">Edit</a>
+                            </li>
+                            <li>
+                                <a href="">More</a>
+                            </li>
+                        </template>
+                    </ListItem>
+                </List>
+            </div>
+        </div>
+        <Footer/>
     </div>
 </template>
-<script>
 
+<script>
+    import Header from '@/components/Header'
+    import Footer from '@/components/Footer'
     import {getSortApi} from '../utils/api'
 
     export default {
+        name: 'Home',
+        components: { Header, Footer
+        },
         data() {
             return {
-                a: 'aaa'
+                sortData:[]
+
             }
         },
+        computed: {},
+        created() {},
         mounted() {
-            this.getSort()
-            console.log('query-sort',this.getSort())
-
+            this.getSortData()
         },
-        methods:{
-            getSort(){ getSortApi().then(res=>{console.log(res)}) }
+        methods: {
+            getSortData(){
+                getSortApi().then(res=>this.sortData = res.data)
+            }
+
+
+
         }
+
 
 
     }
 </script>
-<style lang="scss">
-    .root {
-        font-size: 20px;
 
-        > .left-list {
-            width: 2rem;
-
-        }
-    }
-
-
+<style lang="scss" scoped>
+    @import "./test.scss";
 </style>
